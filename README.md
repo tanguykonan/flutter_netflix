0d70e87582127485a3f4d92755fa77fdaac7998b
Ulrich Tanguy <u.tanguykonan@gmail.com>
2026-04-17 17:48:36 +0000

    Logo + Loading page

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
 # flutter_netflix
 
 A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Learn Flutter](https://docs.flutter.dev/get-started/learn-flutter)
-- [Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Flutter learning resources](https://docs.flutter.dev/reference/learning-resources)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+this project is for educational purposes. 
+## Additional Packages
+- https://pub.dev/packages/flutter_launcher_icons/install
+- https://pub.dev/packages/flutter_native_splash/install
+- https://pub.dev/packages/lottie/install
---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix/pages/home_page.dart';
+import 'package:lottie/lottie.dart';
+
+class MyLoadingPage extends StatefulWidget {
+  const MyLoadingPage({super.key});
+
+  @override
+  State<MyLoadingPage> createState() => _MyLoadingPageState();
+}
+
+class _MyLoadingPageState extends State<MyLoadingPage> {
+
+
+  // Methode qui execute notre loadanimation quant la page est initialisé
+  @override
+  void initState(){
+    super.initState();
+    loadAnimation();
+  }
+
+  // (Methode 2) Fonction de timer qui execute la fonction (Methode1) après un délais
+  Future<Timer> loadAnimation() async {
+    return Timer(
+      const Duration(seconds: 3),
+      onLoading
+    );
+  }
+
+  // Fonction qui rédirige l'utilisateur à la page d'accueil (quant l'animation est finie)
+  /*void onLoading() {
+    Navigator.pushNamed(
+        context,
+        '/home'
+    );
+  }*/
+  // Avec pushNamed, la page home obtient un icon button de retour par défaut
+  void onLoading() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => const MyHomePage())
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Lottie.asset(
+            "res/assets/lotties/netflix.json",
+          repeat: false // Ne pas répéter l'animations
+        ),
+      ),
+    );
+  }
+}
